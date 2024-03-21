835de477eea87d1af3cf6ff6ccf5b13b376bf579
heera9331 <heera9331@gmail.com>
2024-03-21 22:54:52 +0530

    font family

--- index.php
+++ index.php
@@ -34,6 +34,7 @@ $totalPages = ceil($totalServices / $pageSize);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <?php include("./headerLinks.php"); ?>
     <title>Sagar Services</title>
+    <link rel="stylesheet" href="./styles/style.css"/>
 </head>
 <body class="">
 <?php include ("./components/header.php"); ?>
